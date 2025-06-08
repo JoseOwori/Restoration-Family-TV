79809362db76a558db7558f7218879627186821d
Jose <114400672+JoseOwori@users.noreply.github.com>
2025-06-08 21:55:08 +0300

    refactor: rename portfolio to programmes and update related references
    Update all instances of 'portfolio' to 'Programmes' across HTML, CSS, JS, and SCSS files to reflect the new naming convention. Remove logo references and clean up unused dropdown menu items.

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@
         $("#preloder").delay(200).fadeOut("slow");
 
         /*------------------
-            Portfolio filter
+            Programmes filter
         --------------------*/
-        $('.portfolio__filter li').on('click', function () {
-            $('.portfolio__filter li').removeClass('active');
+        $('.Programmes__filter li').on('click', function () {
+            $('.Programmes__filter li').removeClass('active');
             $(this).addClass('active');
         });
-        if ($('.portfolio__gallery').length > 0) {
-            var containerEl = document.querySelector('.portfolio__gallery');
+        if ($('.Programmes__gallery').length > 0) {
+            var containerEl = document.querySelector('.Programmes__gallery');
             var mixer = mixitup(containerEl);
         }
     });
